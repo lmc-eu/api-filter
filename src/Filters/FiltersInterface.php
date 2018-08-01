5fedb18492e762e54dfa5b10d70dc876bd3bfab6
Petr Chromec <petr.chromec@lmc.eu>
2018-08-01 12:00:56 +0200

    Use filter applicator in filters to apply a single filter from one method

--- src/Filters/FiltersInterface.php
+++ src/Filters/FiltersInterface.php
@@ -4,13 +4,14 @@ namespace Lmc\ApiFilter\Filters;
 
 use Lmc\ApiFilter\Applicator\ApplicatorInterface;
 use Lmc\ApiFilter\Entity\Filterable;
+use Lmc\ApiFilter\Service\FilterApplicator;
 
 interface FiltersInterface extends \IteratorAggregate
 {
     /**
      * Apply all filters to given filterable
      */
-    public function applyAllTo(ApplicatorInterface $applicator, Filterable $filterable): Filterable;
+    public function applyAllTo(Filterable $filterable, FilterApplicator $filterApplicator): Filterable;
 
     public function getPreparedValues(ApplicatorInterface $applicator): array;
 }
